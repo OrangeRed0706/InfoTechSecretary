985b774565d518a5e4dc7d1353fb53330970d09d
Lynn <lynnmaxwell0706@gmail.com>
2023-12-12 22:35:10 +0800

    InitialCreate database

---
+++ InfoTechSecretary.Database/Migrations/20231212143447_InitialCreate.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace InfoTechSecretary.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence<int>(
+                name: "blog_id_seq");
+
+            migrationBuilder.CreateSequence<int>(
+                name: "post_id_seq");
+
+            migrationBuilder.CreateSequence<int>(
+                name: "tag_id_seq");
+
+            migrationBuilder.CreateTable(
+                name: "Blog",
+                columns: table => new
+                {
+                    BlogId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    Provider = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Url = table.Column<string>(type: "varchar(255)", nullable: false),
+                    IconUrl = table.Column<string>(type: "varchar(500)", nullable: false),
+                    CreatedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    IsEnabled = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blog", x => x.BlogId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tag",
+                columns: table => new
+                {
+                    TagId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    Name = table.Column<string>(type: "varchar(255)", nullable: false),
+                    CreatedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tag", x => x.TagId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Post",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    Provider = table.Column<int>(type: "integer", nullable: false),
+                    Title = table.Column<string>(type: "varchar", nullable: false),
+                    Time = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Description = table.Column<string>(type: "varchar", nullable: false),
+                    TranslatedDescription = table.Column<string>(type: "varchar", nullable: false),
+                    Link = table.Column<string>(type: "varchar", nullable: false),
+                    IconUrl = table.Column<string>(type: "varchar", nullable: false),
+                    CreatedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    Tags = table.Column<string>(type: "varchar", nullable: false),
+                    IsProcessed = table.Column<bool>(type: "boolean", nullable: false),
+                    IsNotified = table.Column<bool>(type: "boolean", nullable: false),
+                    BlogId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => x.PostId);
+                    table.ForeignKey(
+                        name: "FK_Post_Blog_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blog",
+                        principalColumn: "BlogId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_Provider",
+                table: "Blog",
+                column: "Provider",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Post_BlogId",
+                table: "Post",
+                column: "BlogId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_Link",
+                table: "Post",
+                column: "Link",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_Provider",
+                table: "Post",
+                column: "Provider");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_Name",
+                table: "Tag",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Post");
+
+            migrationBuilder.DropTable(
+                name: "Tag");
+
+            migrationBuilder.DropTable(
+                name: "Blog");
+
+            migrationBuilder.DropSequence(
+                name: "blog_id_seq");
+
+            migrationBuilder.DropSequence(
+                name: "post_id_seq");
+
+            migrationBuilder.DropSequence(
+                name: "tag_id_seq");
+        }
+    }
+}
